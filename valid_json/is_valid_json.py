edcf8264a05be1fbf518746d2b912d0455bee11f
NEO <neo.one.mail@gmail.com>
2021-03-13 08:18:11 +0200

    add valid json

---
+++ valid_json/is_valid_json.py
@@ -0,0 +1,18 @@
+import os 
+import sys
+import json
+
+if len(sys.argv) > 1:
+	if os.path.exists(sys.argv[1]):
+		try:
+			file = open(sys.argv[1], 'r')
+			json.load(file)
+			file.close()
+			print(f'{sys.argv[1]} is a valid json file')
+		except:
+			print(f'{sys.argv[1]} is an invalid json file')
+		
+	else:
+		print(f'Can NOT find {sys.argv[1]}')
+else:
+	print('Correct usage => is_valid_json.py <file>')
